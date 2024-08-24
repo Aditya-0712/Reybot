272c935871873d95ca89b9868730d029adb995f0
Aditya Batgeri <adityabatgeri@gmail.com>
2024-08-24 20:45:56 +0530

    right done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,12 @@
 import "./hero.css";
 import Left from "./Left";
+import Right from "./right";
 
 function Hero(){
     return (
         <div className="hero">
             <Left />
+            <Right />
         </div>
     );
 }
---
+++ src/components/hero/right.jsx
@@ -0,0 +1,16 @@
+import "./right.css";
+import whatsapp from "../../images/forHero/whatsapp.png";
+import chat from "../../images/forHero/chat.png";
+import chat3d from "../../images/forHero/chat3d.png";
+
+function Right(){
+    return (
+        <div className="right">
+            <img className="whatsapp" src={whatsapp} alt="whatsapp" /> 
+            <img className="chatpic" src={chat} alt="chat picture" />
+            <img className="chat3d" src={chat3d} alt="chat emoji" />
+        </div>
+    );
+}
+
+export default Right;
