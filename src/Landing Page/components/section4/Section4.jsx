d3e8f011dbf98558cb225e3b6dfc8c3cc63299c9
Aditya Batgeri <adityabatgeri@gmail.com>
2024-08-25 17:50:57 +0530

    section 4 px to vw

--- src/Landing Page/components/section4/Section4.jsx
+++ src/Landing Page/components/section4/Section4.jsx
@@ -6,7 +6,7 @@ function Section4(){
     return (
         <div className="section4">
             <p className="heading">A Complete Soluion for</p>
-            <p className="heading" style={{color:"#0DD171", marginTop:"10px"}}>Marketing & Customer Care</p>
+            <p className="heading" style={{color:"#0DD171", marginTop:"0.657vw"}}>Marketing & Customer Care</p>
             <HeaderCard />
             <MainCard />
         </div>
