7dd0350853911d5fdd524f33ad43a6402a6dc83a
Aditya Batgeri <adityabatgeri@gmail.com>
2024-08-25 22:02:30 +0530

    section 6 px to vw done

--- src/Landing Page/components/section6/Section6.jsx
+++ src/Landing Page/components/section6/Section6.jsx
@@ -16,7 +16,7 @@ function Section6(){
                 <p className="desc">Partner with a trusted leader in global solutions. Our track record of success with multinational corporations demonstrates our reliability, innovation, and commitment to excellence. Join the ranks of esteemed global companies who rely on us for cutting-edge solutions and unparalleled support</p>
                 <div className="box">
                     <div><p>10</p><p>World Wide Clients</p></div>
-                    <div style={{marginLeft:"100px"}}><p>25 +</p><p style={{marginLeft:"10px"}}>Bots</p></div>
+                    <div style={{marginLeft:"6.572vw"}}><p>25 +</p><p style={{marginLeft:"0.657vw"}}>Bots</p></div>
                 </div>
             </div>
             <img className="sec6pt3" src={vector2} alt="vector" />
