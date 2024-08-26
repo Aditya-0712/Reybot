43000075116f7df6210b0ed3ddc9f65707e62647
Aditya Batgeri <adityabatgeri@gmail.com>
2024-08-26 13:38:46 +0530

    section 9 bg added

--- src/Landing Page/components/section9/Section9.jsx
+++ src/Landing Page/components/section9/Section9.jsx
@@ -2,10 +2,13 @@ import "./section9.css";
 import { Link } from "react-router-dom";
 import right from "../../images/forSection9/Arrow Right.svg";
 import Faq from "./Faq";
+import oval1 from "../../images/forSection9/oval1.png";
+import oval2 from "../../images/forSection9/oval2.png";
 
 function Section9(){
     return (
         <div className="section9">
+            <img src={oval1} className="oval1" alt="oval" />
             <p className="heading">OUR FAQS</p>
             <p className="title">Frequently Asked<br></br>Questions</p>
             <Link to="" className="link">
@@ -13,6 +16,7 @@ function Section9(){
                 <img src={right} alt="arrow" />
             </Link>
             <Faq />
+            <img src={oval2} className="oval2" alt="oval" />
         </div>
     );
 }
